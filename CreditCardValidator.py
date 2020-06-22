5f9713f401a77ceb6da6ab77ca54737b86b9019d
charlesumesi <54457600+charlesumesi@users.noreply.github.com>
2020-06-22 23:16:00 +0100

    Add files via upload

---
+++ CreditCardValidator.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 16 Feb 2020; edited on 22 Jun 2020
+
+@author: Charles Umesi
+"""
+
+def credit_card_validator():
+    
+    '''Based on the Luhn formula'''
+    '''This version uses https://en.wikipedia.org/wiki/Luhn_algorithm'''
+    
+    # Entering of credit card number
+    enter_number = input('Enter credit card number (with no spaces) : ')
+    
+    # Implementing formula
+    l = [k for k in enter_number]
+    m = l[::-1]
+    n = [str(2*int(i)) for i in m[1::2]]
+    o = m[0::2]
+    p = ''.join(n)
+    q = [j for j in p]
+       
+    r = 9*(sum(list(map(int, q))) + sum(list(map(int, o))))
+        
+    if r%10 == 0: print('The number entered is a valid credit card number.')
+    else: print('The number entered is not a valid credit card number.')
+    del(l,m,n,o,p,q)
+
+if __name__ == '__main__':
+    credit_card_validator()
